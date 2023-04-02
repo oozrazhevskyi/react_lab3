f040ac67b11e5d4fb64ae3cad212a8a7c99bb97c
My Name <i@example.com>
2023-04-02 21:26:28 +0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import CarouselBoxHk from "../components/CarouselBoxHk"
+
+export default class Home extends Component {
+  render() {
+    return (
+      <CarouselBoxHk/>
+    )
+  }
+}
